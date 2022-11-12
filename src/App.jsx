4f9209c99a1d82e8983c1279b2c103ceb8c3ad59
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-11-12 20:43:12 +0900

    #34 feature/privateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, Login, Signup, Cart, Clothes, Food, Toy, Admin } from "./pages";
+import {
+  Home,
+  Login,
+  Signup,
+  Cart,
+  Clothes,
+  Food,
+  Toy,
+  Admin,
+  PrivateRoute,
+} from "./pages";
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
 import "./App.css";
@@ -27,7 +37,14 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/admin/*" element={<Admin />} />
+          <Route
+            path="/admin/*"
+            element={
+              <PrivateRoute>
+                <Admin />
+              </PrivateRoute>
+            }
+          />
           <Route path="/cart" element={<Cart />} />
           <Route path="/clothes" element={<Clothes />} />
           <Route path="/food" element={<Food />} />
