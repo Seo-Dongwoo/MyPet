358ee768284ac3ff6ca5e717cc1955682b6af2f1
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-11-21 17:49:48 +0900

    feature/home-slider

---
+++ src/components/home/MainSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Slider from "./slider/Slider";
+
+const MainSection = () => {
+  return (
+    <>
+      <Slider />
+    </>
+  );
+};
+
+export default MainSection;
---
+++ src/components/home/slider/Slider.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { sliderData } from "./slider-data";
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideLength = sliderData.length;
+
+  const autoScroll = true;
+  let slideInterval;
+  let intervalTime = 5000;
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+  };
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+  };
+
+  useEffect(() => {
+    setCurrentSlide(0);
+  }, []);
+
+  useEffect(() => {
+    if (autoScroll) {
+      const auto = () => {
+        slideInterval = setInterval(nextSlide, intervalTime);
+      };
+      auto();
+    }
+    return () => clearInterval(slideInterval);
+  }, [currentSlide, slideInterval, autoScroll]);
+
+  return (
+    <SlideContainer>
+      <LeftArrow onClick={prevSlide} />
+      <RightArrow onClick={nextSlide} />
+      {sliderData.map((slide, index) => {
+        return (
+          <CurrentSlide key={index}>
+            {index === currentSlide && (
+              <>
+                <Image src={slide.image} alt="slide" />
+              </>
+            )}
+          </CurrentSlide>
+        );
+      })}
+    </SlideContainer>
+  );
+};
+
+const SlideContainer = styled.div`
+  width: 100%;
+  height: 450px;
+  position: relative;
+  overflow: hidden;
+  background-color: var(--color-dark);
+`;
+
+const LeftArrow = styled(AiOutlineArrowLeft)`
+  position: absolute;
+  border: 2px solid #01bf71;
+  border-radius: 10%;
+  background: transparent;
+  color: #01bf71;
+  width: 2.5rem;
+  height: 2.5rem;
+  cursor: pointer;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 2;
+  left: 1.5rem;
+  &:hover {
+    background: #fff;
+  }
+`;
+
+const CurrentSlide = styled.div``;
+
+const RightArrow = styled(AiOutlineArrowRight)`
+  position: absolute;
+  border: 2px solid #01bf71;
+  border-radius: 10%;
+  background: transparent;
+  color: #01bf71;
+  width: 2.5rem;
+  height: 2.5rem;
+  cursor: pointer;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 2;
+  right: 1.5rem;
+  &:hover {
+    background: #fff;
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 450px;
+`;
+
+export default Slider;
