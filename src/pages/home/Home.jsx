8a882fa26b1cb31ca5d15a322611aa8bab233215
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-11-27 18:16:15 +0900

    feature/footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import MainSection from "../../components/home/MainSection";
-import Products from "../../components/home/products/Products";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <MainSection />
-      <Products />
+      <Footer />
     </div>
   );
 };
