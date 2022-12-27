7956726464077d8d75ef64262a910a4d98816ecb
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-12-27 21:40:11 +0900

    feature/order-products

--- src/components/cart/orderDetails/OrderButton.jsx
+++ src/components/cart/orderDetails/OrderButton.jsx
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { addOrderInitiate } from "../../../redux/modules/actions/orderActions";
 import { useNavigate } from "react-router-dom";
-import { unsubscribe } from "../../../redux/modules/actions/orderActions";
 import { v4 } from "uuid";
 
 const OrderButton = ({ checkItems, address, totalPrice, detailAddress }) => {
-  const { orderItems } = useSelector((state) => state.orderProduct);
-  const [data, setData] = useState([]);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const orderItemsList = checkItems.map((item) => item);
-  // const orderPath = orderItems.map((item) =>
-  //   item.orderItemsList.map((item) => item.token)
-  // );
-
-  const orderPath = orderItems.map((item) => item.id);
 
   const handleSubmit = () => {
     if (orderItemsList && address && detailAddress) {
@@ -38,12 +30,6 @@ const OrderButton = ({ checkItems, address, totalPrice, detailAddress }) => {
   };
 
   useEffect(() => {}, [orderItemsList, totalPrice, address, detailAddress]);
-
-  // useEffect(() => {
-  //   return () => {
-  //     dispatch(unsubscribe(setData));
-  //   };
-  // }, []);
 
   return (
     <Container>
